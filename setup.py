06e8fbdecb40785db61760e66cc75aaf917d8384
Mattijs Ugen <akaidiot@users.noreply.github.com>
2017-12-01 15:57:40 +0100

    Add minimal setup.py, compile requirements file

---
+++ setup.py
@@ -0,0 +1,18 @@
+import codecs
+from setuptools import setup
+
+
+dependencies = [
+    'pyyaml',
+]
+
+
+setup(
+    name='configuration',
+    version='0.0',
+    author='Mattijs Ugen',
+    author_email=codecs.encode('nxnvqvbg@hfref.abercyl.tvguho.pbz', 'rot-13'),
+    description='',
+    py_modules=['configuration'],
+    install_requires=dependencies,
+)
