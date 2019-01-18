ff94e336346996f3e6febe6f99be8ab2ebc75715
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2019-01-18 16:11:06 +0100

    Redefine default LOAD_ORDER as system, user, application, environment

--- confidence.py
+++ confidence.py
@@ -522,26 +522,16 @@ _LOADERS = {
 }
 
 
-# ordered sequence of name templates to load, in increasing significance
-LOAD_ORDER = (
-    # system-wide locations
-    read_xdg_config_dirs,
-    '/etc/{name}.{extension}',
-    '/Library/Preferences/{name}.{extension}',
-    partial(read_envvar_dir, 'PROGRAMDATA'),
+def loaders(*specifiers):
+    return tuple(chain.from_iterable(
+        _LOADERS[locality] for locality in specifiers
+    ))
 
-    # user-local locations
-    read_xdg_config_home,
-    '~/Library/Preferences/{name}.{extension}',
-    partial(read_envvar_dir, 'APPDATA'),
-    partial(read_envvar_dir, 'LOCALAPPDATA'),
-    '~/.{name}.{extension}',
 
-    # application-local locations
-    './{name}.{extension}',
-    read_envvar_file,
-    read_envvars,
-)
+LOAD_ORDER = loaders(Locality.system,
+                     Locality.user,
+                     Locality.application,
+                     Locality.environment)
 
 
 def load_name(*names, load_order=LOAD_ORDER, extension='yaml'):
