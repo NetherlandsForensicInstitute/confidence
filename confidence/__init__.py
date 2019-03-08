8ef5ab201911bcf38d076bc5d8df342606b4a025
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2019-03-08 15:55:30 +0100

    Define confidence.__all__ to contain everything considered to be user-facing API

--- confidence/__init__.py
+++ confidence/__init__.py
@@ -1,3 +1,10 @@
 from confidence.exceptions import ConfigurationError, ConfiguredReferenceError, MergeConflictError, NotConfiguredError
 from confidence.io import load, loadf, loads
 from confidence.models import Configuration, Missing, NotConfigured
+
+
+__all__ = (
+    'ConfigurationError', 'ConfiguredReferenceError', 'MergeConflictError', 'NotConfiguredError',
+    'load', 'loadf', 'loads',
+    'Configuration', 'Missing', 'NotConfigured',
+)
