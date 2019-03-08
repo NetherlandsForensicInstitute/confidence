8107c40aae01f68d8192f2c4230b1242613262c0
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2019-03-08 15:26:38 +0100

    Move Exception types to confidence.exceptions

---
+++ confidence/exceptions.py
@@ -0,0 +1,30 @@
+class ConfigurationError(KeyError):
+    pass
+
+
+class MergeConflictError(ConfigurationError):
+    """
+    Error raised during loading configuration sources that conflict internally.
+    """
+    def __init__(self, *args, key):
+        super().__init__(*args)
+        self.conflict = key
+
+
+class NotConfiguredError(ConfigurationError):
+    """
+    Error raised when a requested configuration key is unavailable and no
+    default / fallback value is provided.
+    """
+    def __init__(self, *args, key):
+        super().__init__(*args)
+        self.key = key
+
+
+class ConfiguredReferenceError(ConfigurationError):
+    """
+    Error raised a referenced configuration key is unavailable.
+    """
+    def __init__(self, *args, key):
+        super().__init__(*args)
+        self.key = key
