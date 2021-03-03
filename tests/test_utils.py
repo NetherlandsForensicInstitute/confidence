317acb642fee42c66f26d7b297f28317adc81a1c
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2021-03-03 16:35:21 +0100

    Fix unit tests for _merge

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,46 +9,58 @@ def test_merge_trivial():
     left = {'key': 'value'}
     right = {'another_key': 42}
 
-    merged = _merge(left, right)
+    merges = list(_merge(left, right))
 
-    assert len(merged) == 2, 'trivial merge of incorrect length'
-    assert merged['key'] == 'value', 'trivial merge supplied wrong value'
-    assert merged['another_key'] == 42, 'trivial merge supplied wrong value'
+    assert len(left) == 2, 'trivial merge of incorrect length'
+    assert left['key'] == 'value', 'trivial merge supplied wrong value'
+    assert left['another_key'] == 42, 'trivial merge supplied wrong value'
+
+    assert len(merges) == 1
+    assert merges[0] == (('another_key',), None)
 
 
 def test_merge_update():
     left = {'first': {'key': 1}}
     right = {'second': {'key': True}}
 
-    merged = _merge(left, right)
+    merges = list(_merge(left, right))
 
-    assert len(merged) == 2, 'update merge of incorrect length'
-    assert len(merged['first']) == len(merged['second']) == 1, 'update merge values of incorrect length'
-    assert merged['first']['key'] == 1, 'update merge supplied wrong value'
-    assert merged['second']['key'], 'update merge supplied wrong value'
+    assert len(left) == 2, 'update merge of incorrect length'
+    assert len(left['first']) == len(left['second']) == 1, 'update merge values of incorrect length'
+    assert left['first']['key'] == 1, 'update merge supplied wrong value'
+    assert left['second']['key'], 'update merge supplied wrong value'
+
+    assert len(merges) == 1
+    assert merges[0] == (('second', 'key'), None)
 
 
 def test_merge_overlap():
     left = {'parent': {'first': 123, 'second': 456}}
     right = {'parent': {'third': 789, 'fourth': True}}
 
-    merged = _merge(left, right)
+    merges = list(_merge(left, right))
 
-    assert len(merged) == 1, 'overlapping merge of incorrect length'
-    assert len(merged['parent']) == 4, 'value in overlapping merge of incorrect length'
-    assert (merged['parent']['second'], merged['parent']['third']) == (456, 789), \
+    assert len(left) == 1, 'overlapping merge of incorrect length'
+    assert len(left['parent']) == 4, 'value in overlapping merge of incorrect length'
+    assert (left['parent']['second'], left['parent']['third']) == (456, 789), \
         'incorrect values in overlapping merge'
+
+    assert len(merges) == 2
+    assert merges[1] == (('parent', 'fourth'), None)
 
 
 def test_merge_equal():
     left = {'parent': {'first': 1, 'second': 2}}
     right = {'parent': {'third': 3, 'first': 1}}  # parent.first == 1 in both operands
 
-    merged = _merge(left, right)
+    merges = list(_merge(left, right))
 
-    assert len(merged) == 1, 'equality merge of incorrect length'
-    assert len(merged['parent']) == 3, 'equality merge value of incorrect length'
-    assert merged['parent']['first'] == 1, 'equality merge supplied wrong value'
+    assert len(left) == 1, 'equality merge of incorrect length'
+    assert len(left['parent']) == 3, 'equality merge value of incorrect length'
+    assert left['parent']['first'] == 1, 'equality merge supplied wrong value'
+
+    assert len(merges) == 1
+    assert merges[0] == (('parent', 'third'), None)
 
 
 def test_merge_conflict():
@@ -56,7 +68,7 @@ def test_merge_conflict():
     right = {'parent': {'third': 3, 'first': 4}}  # parent.first differs
 
     try:
-        merged = _merge(left, right)
+        _ = list(_merge(left, right))
     except Exception as e:
         assert 'parent.first' in str(e), "conflict error didn't specify conflicting key"
     else:
@@ -67,11 +79,18 @@ def test_merge_conflict_overwrite():
     left = {'parent': {'first': 1, 'second': 2}}
     right = {'parent': {'third': 3, 'first': 4}}  # parent.first differs
 
-    merged = _merge(left, right, conflict=_Conflict.overwrite)
+    merges = list(_merge(left, right, conflict=_Conflict.overwrite))
 
-    assert len(merged) == 1
-    assert len(merged['parent']) == 3
-    assert merged['parent']['first'] == 4
+    assert len(left) == 1
+    assert len(left['parent']) == 3
+    assert left['parent']['first'] == 4
+
+    assert len(merges) == 2
+    assert merges[0] == (('parent', 'third'), None)
+    assert merges[1] == (('parent', 'first'), None)
+
+
+# TODO: add tests with origins
 
 
 def test_split_none():
