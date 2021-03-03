5241e89748f27e3ba8ac9c46f750ee66f0c58476
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2021-03-03 16:40:47 +0100

    Add line comments to defined type aliases

--- confidence/types.py
+++ confidence/types.py
@@ -1,8 +1,13 @@
 import typing
 
 
+# expected type for a source of a Configuration object (typically a dict in practise)
 ConfigurationSource = typing.MutableMapping[str, typing.Any]
 
+
+# the key under which the origin of a configured value is tracked, a tuple with strings ('path steps')
 Key = typing.Tuple[str, ...]
+# an optional origin of a configured value
 Origin = typing.Optional[str]
+# a collection of origins for configured values, mapping Keys to Origins
 KeyOrigins = typing.MutableMapping[Key, Origin]
