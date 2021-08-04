504001a73c5699378a74e33598636e71fb512c6f
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2021-08-04 15:28:58 +0200

    Expose dump from confidence.*

--- confidence/__init__.py
+++ confidence/__init__.py
@@ -1,10 +1,10 @@
 from confidence.exceptions import ConfigurationError, ConfiguredReferenceError, MergeConflictError, NotConfiguredError
-from confidence.io import DEFAULT_LOAD_ORDER, dumpf, dumps, load, load_name, loaders, loadf, loads, Locality
+from confidence.io import DEFAULT_LOAD_ORDER, dump, dumpf, dumps, load, load_name, loaders, loadf, loads, Locality
 from confidence.models import Configuration, Missing, NotConfigured
 
 
 __all__ = (
     'ConfigurationError', 'ConfiguredReferenceError', 'MergeConflictError', 'NotConfiguredError',
-    'DEFAULT_LOAD_ORDER', 'dumpf', 'dumps', 'load', 'load_name', 'loaders', 'loadf', 'loads', 'Locality',
+    'DEFAULT_LOAD_ORDER', 'dump', 'dumpf', 'dumps', 'load', 'load_name', 'loaders', 'loadf', 'loads', 'Locality',
     'Configuration', 'Missing', 'NotConfigured',
 )
