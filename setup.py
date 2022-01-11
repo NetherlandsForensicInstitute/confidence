15f16be8bc5b5838157550f5ce61fbaa500b67e6
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2022-01-11 16:02:25 +0100

    Update Python version classifiers in package metadata

--- setup.py
+++ setup.py
@@ -47,11 +47,10 @@ setup(
 
     classifiers=(
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Utilities',
     )
 )
