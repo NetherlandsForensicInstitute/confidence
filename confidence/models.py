878b859f2e38deb8217b96ce51eda0ac157e9e24
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2022-11-11 13:26:07 +0100

    Use _unwrap in Configuration init

--- confidence/models.py
+++ confidence/models.py
@@ -64,12 +64,9 @@ class Configuration(Mapping):
         self._source: typing.MutableMapping[str, typing.Any] = {}
         for source in sources:
             if source:
-                while isinstance(source, Configuration):
-                    # _merge will walk source.items(), using source.get(), avoid resolving references now
-                    source = source._source
-
                 # merge values from source into self._source, overwriting any corresponding keys
-                merge(self._source, split_keys(source, colliding=_COLLIDING_KEYS), conflict=Conflict.OVERWRITE)
+                # unwrap the source to make sure we're dealing with simple types
+                merge(self._source, split_keys(_unwrap(source), colliding=_COLLIDING_KEYS), conflict=Conflict.OVERWRITE)
 
     def _wrap(self, value: typing.Mapping[str, typing.Any]) -> 'Configuration':
         # create an instance of our current type, copying 'configured' properties / policies
