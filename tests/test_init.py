7c51190f2ff404acad9758f5cdf0663dfb60a480
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2022-11-11 13:48:33 +0100

    Add test to verify recursive unwrapping

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,8 +1,25 @@
-from confidence import Configuration, NotConfigured
+from confidence import Configuration, dumps, loads, NotConfigured
+from confidence.models import ConfigurationSequence
 
 
 def test_constructor_defaults():
     subject = Configuration()
+
     assert subject._missing == NotConfigured
     assert len(subject) == len(subject._source) == 0
     assert list(subject) == []
+
+
+def test_wrapped_source():
+    left = Configuration({'a': 'a', 'b': [2, 2]})
+    right = Configuration({'a': [1], 'b': Configuration({'c': 42})})
+
+    subject = Configuration({'left': left, 'right': right})
+
+    assert not isinstance(subject._source['left'], Configuration)
+    assert not isinstance(subject._source['right']['b'], Configuration)
+    assert not isinstance(subject._source['right']['a'], ConfigurationSequence)
+
+    assert subject.right.b.c == 42
+    assert len(left.b) == len(subject.left.b) == 2
+    assert subject._source == loads(dumps(subject))._source
