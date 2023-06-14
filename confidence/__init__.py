19c02c2498a13b1f0daabe8dab4041277a3e49f8
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2023-06-14 15:33:01 +0200

    Add unwrap to confidence.__all__, explicitly adding it to the public API

--- confidence/__init__.py
+++ confidence/__init__.py
@@ -2,13 +2,13 @@ import logging
 
 from confidence.exceptions import ConfigurationError, ConfiguredReferenceError, MergeConflictError, NotConfiguredError
 from confidence.io import DEFAULT_LOAD_ORDER, dump, dumpf, dumps, load, load_name, loaders, loadf, loads, Locality
-from confidence.models import Configuration, Missing, NotConfigured
+from confidence.models import Configuration, Missing, NotConfigured, unwrap
 
 
 __all__ = (
     'ConfigurationError', 'ConfiguredReferenceError', 'MergeConflictError', 'NotConfiguredError',
     'DEFAULT_LOAD_ORDER', 'dump', 'dumpf', 'dumps', 'load', 'load_name', 'loaders', 'loadf', 'loads', 'Locality',
-    'Configuration', 'Missing', 'NotConfigured',
+    'Configuration', 'Missing', 'NotConfigured', 'unwrap',
 )
 
 
