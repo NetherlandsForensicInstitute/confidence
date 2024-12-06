769eb03c3bed4a1021367a4456c5c17c589d999b
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2024-12-06 17:12:31 +0100

    Add parameterizable implementations of Secrets.{matches,resolve}

--- confidence/secrets.py
+++ confidence/secrets.py
@@ -1,4 +1,12 @@
+import logging
 import typing
+
+
+LOG = logging.getLogger(__name__)
+
+
+DEFAULT_SINGLE_KEY_IDENTIFIER = '$secret'
+DEFAULT_SINGLE_KEY_ARGS = ('service', 'username')
 
 
 @typing.runtime_checkable
@@ -14,3 +22,23 @@ class Secrets(typing.Protocol):
 class SecretCallback(typing.Protocol):
     def __call__(self, *args: str) -> str | None:
         ...
+
+
+def is_single_key_secret(value: typing.Mapping[str, typing.Any],
+                         *,
+                         key: str) -> bool:
+    return len(value) == 1 and key in value
+
+
+def resolve_n_key_secret_callback(value: typing.Mapping[str, typing.Any],
+                                  *,
+                                  callback: SecretCallback,
+                                  single_key: str,
+                                  args: typing.Iterable[str]) -> str | None:
+    try:
+        secret = value[single_key]
+        return callback(*(secret[arg] for arg in args))
+    except KeyError as e:
+        missing_key = e.args[0] if e.args[0] == single_key else f'{single_key}.{e.args[0]}'
+        LOG.warning(f'resolving secret failed, missing key {missing_key}')
+        raise
