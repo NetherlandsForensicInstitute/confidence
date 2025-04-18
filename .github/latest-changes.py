104f5f55fb0fcb33ca2445845117cc41f7874e1c
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2025-04-18 13:53:06 +0200

    Add line comments to latest-changes.py

--- .github/latest-changes.py
+++ .github/latest-changes.py
@@ -6,9 +6,11 @@ if os.environ.get('GITHUB_ACTIONS'):
     root = Path(__file__).parent.parent
     with open(root / 'CHANGES.md', 'rt') as changes, open(root / 'LATEST-CHANGES.md', 'wt') as latest_changes:
         line = None
+        # read past content until we reach a line starting with "- "
         while not (line := next(changes)).startswith('- '):
             pass
 
+        # write the first matching line, and all consecutive matching lines
         latest_changes.write(line)
         while (line := next(changes)).startswith('- '):
             latest_changes.write(line)
