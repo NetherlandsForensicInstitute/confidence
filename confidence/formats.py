481ab0ad02fcc7ddafc3d5077f30c39ce7c6187a
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2025-05-31 16:07:35 +0200

    Add a JSON format for good measure

--- confidence/formats.py
+++ confidence/formats.py
@@ -1,3 +1,4 @@
+import json
 import typing
 from os import PathLike
 from pathlib import Path
@@ -17,6 +18,16 @@ class Format(typing.Protocol):
     def loadf(self, fpath: typing.Union[str, PathLike], encoding: str = 'utf-8') -> typing.Any:
         with Path(fpath).expanduser().open('rt', encoding=encoding) as fp:
             return self.load(fp)
+
+
+class JSON(Format):
+    extension: typing.Optional[str] = '.json'
+
+    def __init__(self, extension: str = '.json'):
+        self.extension = extension
+
+    def loads(self, string: str) -> typing.Any:
+        return json.loads(string)
 
 
 class YAML(Format):
