44e16f1724457b4e8f3967537890d4dbac697c30
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2025-05-31 16:07:05 +0200

    Add a YAML format

--- confidence/formats.py
+++ confidence/formats.py
@@ -2,6 +2,8 @@ import typing
 from os import PathLike
 from pathlib import Path
 
+import yaml
+
 
 class Format(typing.Protocol):
     extension: typing.Optional[str] = None
@@ -15,3 +17,13 @@ class Format(typing.Protocol):
     def loadf(self, fpath: typing.Union[str, PathLike], encoding: str = 'utf-8') -> typing.Any:
         with Path(fpath).expanduser().open('rt', encoding=encoding) as fp:
             return self.load(fp)
+
+
+class YAML(Format):
+    extension: typing.Optional[str] = '.yaml'
+
+    def __init__(self, extension: str = '.yaml'):
+        self.extension = extension
+
+    def loads(self, string: str) -> typing.Any:
+        return yaml.safe_load(string)
