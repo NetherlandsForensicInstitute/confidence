6c5b9ea5efbf1457edb5a1bac0badf2775a6d488
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2025-05-31 16:06:27 +0200

    Declare a Format protocol to handle {string, path, file} → value

---
+++ confidence/formats.py
@@ -0,0 +1,17 @@
+import typing
+from os import PathLike
+from pathlib import Path
+
+
+class Format(typing.Protocol):
+    extension: typing.Optional[str] = None
+
+    def load(self, fp: typing.TextIO) -> typing.Any:
+        return self.loads(fp.read())
+
+    def loads(self, string: str) -> typing.Any:
+        raise NotImplementedError
+
+    def loadf(self, fpath: typing.Union[str, PathLike], encoding: str = 'utf-8') -> typing.Any:
+        with Path(fpath).expanduser().open('rt', encoding=encoding) as fp:
+            return self.load(fp)
