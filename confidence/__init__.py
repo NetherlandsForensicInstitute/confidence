3bedf4eeddaf662a194c86eec9608d45e9dd8666
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2025-10-03 16:55:59 +0200

    Make tomlkit a default dependency, expose from root module

--- confidence/__init__.py
+++ confidence/__init__.py
@@ -2,41 +2,38 @@ import logging
 from collections.abc import Sequence
 
 from confidence.exceptions import ConfigurationError, ConfiguredReferenceError, MergeConflictError, NotConfiguredError
-from confidence.formats import JSON, YAML, Format, _toml_available
+from confidence.formats import JSON, TOML, YAML, Format
 from confidence.io import DEFAULT_LOAD_ORDER, Locality, dump, dumpf, dumps, load, load_name, loaders, loadf, loads
 from confidence.models import Configuration, Missing, NotConfigured, merge, unwrap
 
 
-__all__: Sequence[str] = (
-    'ConfigurationError',
-    'ConfiguredReferenceError',
-    'DEFAULT_LOAD_ORDER',
-    'Format',
-    'JSON',
-    'MergeConflictError',
-    'NotConfiguredError',
-    'YAML',
-    'dump',
-    'dumpf',
-    'dumps',
-    'load',
-    'load_name',
-    'loaders',
-    'loadf',
-    'loads',
-    'Locality',
-    'Configuration',
-    'merge',
-    'Missing',
-    'NotConfigured',
-    'unwrap',
+__all__: Sequence[str] = sorted(
+    {
+        'Configuration',
+        'ConfigurationError',
+        'ConfiguredReferenceError',
+        'DEFAULT_LOAD_ORDER',
+        'Format',
+        'JSON',
+        'Locality',
+        'MergeConflictError',
+        'Missing',
+        'NotConfigured',
+        'NotConfiguredError',
+        'TOML',
+        'YAML',
+        'dump',
+        'dumpf',
+        'dumps',
+        'load',
+        'load_name',
+        'loaders',
+        'loadf',
+        'loads',
+        'merge',
+        'unwrap',
+    }
 )
-
-
-if _toml_available:
-    from confidence.formats import TOML
-
-    __all__ = (*__all__, 'TOML')
 
 
 # default confidence' loggers to silence, can be overridden from logging later if needed
