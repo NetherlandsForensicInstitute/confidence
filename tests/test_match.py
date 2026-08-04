1066a30d499277b9be4c9392e64e130d17b2ec3f
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-08-04 16:08:59 +0200

    Test match behaviour of ConfigurationSequence

--- tests/test_match.py
+++ tests/test_match.py
@@ -59,3 +59,51 @@ def test_match_mapping_complex():
             assert config.a.b == twelve == 12 and config.a.e.f.g == forty_two == 42
         case _:
             pytest.fail()
+
+
+def test_match_sequence_simple():
+    config = Configuration({'a': [1, 2, 3], 'b': []})
+
+    match config:
+        case {'a': [*nums]}:
+            # TODO: a ConfigurationSequence is not equal to a similar sequence
+            #       (though a list and a tuple are not equal either, so maybe that's ok?)
+            assert list(config.a) == nums == [1, 2, 3]
+        case _:
+            pytest.fail()
+
+    match config:
+        case {'b': [] as empty}:
+            # TODO: two empty ConfigurationSequences are not equal
+            assert list(config.b) == list(empty)
+        case _:
+            pytest.fail()
+
+    match config.a:
+        case [1, 2, 3]:
+            pass
+        case _:
+            pytest.fail()
+
+
+def test_match_sequence_complex():
+    config = Configuration({'a': [1, 2, '${b.c}'], 'b': {'c': 3, 'd': '${a}'}})
+
+    match config:
+        case {'a': [1, 2, c]}:
+            assert c == 3
+        case _:
+            pytest.fail()
+
+    match config:
+        case {'b.d': [head, *tail]}:
+            assert head == 1
+            assert tail == [2, 3]
+        case _:
+            pytest.fail()
+
+    match config:
+        case {'a': Sequence(), 'b.d': [1, 2, 3]}:
+            pass
+        case _:
+            pytest.fail()
