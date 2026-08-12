c36155d8eef9056216d421fbf6dce8aefd3b1f87
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-08-12 11:23:43 +0200

    Define an assert_loadf_paths helper to assert calls to a mocked loadf

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,4 +1,8 @@
 from itertools import pairwise
+from pathlib import Path
+from unittest.mock import call
+
+from confidence import NOT_CONFIGURED, YAML
 
 
 def equivalent(a, b, *more):
@@ -13,3 +17,24 @@ def equivalent(a, b, *more):
             return all(equivalent(item_a, item_b) for item_a, item_b in zip(items_a, items_b, strict=True))
         case _:
             return a == b
+
+
+def assert_loadf_paths(loadf, paths, **repeated):
+    def to_path_args(p):
+        match p:
+            case [*ps]:
+                # multiple paths for a single call, Path() 'm all
+                return [Path(p) for p in ps]
+            case str() | Path():
+                # single path for a single call, wrap the single path into a list for use with *args
+                return [Path(p)]
+            case _:
+                raise TypeError
+
+    # use the declared defaults for load_name unless explicitly supplied
+    repeated.setdefault('format', YAML)
+    repeated.setdefault('default', NOT_CONFIGURED)
+
+    # construct mock call objects as they're expected to be called according to our own args
+    calls = [call(*to_path_args(path), **repeated) for path in paths]
+    loadf.assert_has_calls(calls, any_order=False)
